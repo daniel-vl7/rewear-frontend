0f7ef1968d27e1c0dd9310bcde11f739ccc874f9
daniel <u202121440@upc.edu.pe>
2023-10-25 02:34:45 -0500

    feat(rewear): added basic structure of project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rewear-frontend';
+  options = [
+    { path: '/home', title: 'Home'},
+    { path: '/shoppers', title: 'Shoppers'},
+    {path: '/retailers',title: 'Retailers'}
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+
+import {PageNotFoundComponent} from './public/pages/page-not-found/page-not-found.component';
+import {RetailerComponent} from './rewear/retailers/pages/retailer/retailer.component';
+import {ShopperComponent} from "./rewear/shoppers/pages/shopper/shopper.component";
+import {HomeComponent} from './public/pages/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PageNotFoundComponent, RetailerComponent, ShopperComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
