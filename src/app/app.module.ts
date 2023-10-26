5c61298857604eb4e6d430ce668325ca760225b4
daniel <u202121440@upc.edu.pe>
2023-10-26 14:09:21 -0500

    feat(retailer-products): added form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,22 @@ import {PageNotFoundComponent} from './public/pages/page-not-found/page-not-foun
 import {RetailerComponent} from './rewear/retailers/pages/retailer/retailer.component';
 import {ShopperComponent} from "./rewear/shoppers/pages/shopper/shopper.component";
 import {HomeComponent} from './public/pages/home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from "@angular/material/card";
+import {MatGridListModule} from '@angular/material/grid-list';
+
+import {ProductsComponent} from './rewear/retailers/components/products/products.component';
 
 @NgModule({
-  declarations: [AppComponent, PageNotFoundComponent, RetailerComponent, ShopperComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule, MatToolbarModule],
+  declarations: [AppComponent, PageNotFoundComponent, RetailerComponent, ShopperComponent, HomeComponent, ProductsComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule, MatToolbarModule,MatSelectModule,MatInputModule,MatFormFieldModule,MatCardModule,MatGridListModule],
   providers: [],
   bootstrap: [AppComponent]
 })
