eaeda4b011026ce283377358799fe2f0ffd1ff14
Adrian Melgar <DaikiMT@outlook.es>
2023-10-31 03:20:28 -0500

    Added shopper components for sidebar testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import {RetailerComponent} from './rewear/retailers/pages/retailer/retailer.comp
 import {ShopperComponent} from "./rewear/shoppers/pages/shopper/shopper.component";
 import {HomeComponent} from './public/pages/home/home.component';
 import {ProductsComponent} from "./rewear/retailers/components/products/products.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
@@ -30,14 +30,16 @@ import {MatExpansionModule} from '@angular/material/expansion';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatBadgeModule} from '@angular/material/badge';
-import { AdminPanelComponent } from './rewear/retailers/components/admin-panel/admin-panel.component';
-import { ChatComponent } from './rewear/retailers/components/chat/chat.component';
-import { EventsComponent } from './rewear/retailers/components/events/events.component';
+import {AdminPanelComponent} from './rewear/retailers/components/admin-panel/admin-panel.component';
+import {ChatComponent} from './rewear/retailers/components/chat/chat.component';
+import {EventsComponent} from './rewear/retailers/components/events/events.component';
 import {ChatServiceService} from "./rewear/retailers/services/chat-service.service";
 import {HttpClientModule} from "@angular/common/http";
+import {SidebarComponent} from './rewear/shoppers/components/sidebar/sidebar.component';
+
 @NgModule({
-  declarations: [AppComponent, PageNotFoundComponent, RetailerComponent, ShopperComponent, HomeComponent, ProductsComponent, AdminPanelComponent, ChatComponent, EventsComponent],
-  imports: [ HttpClientModule,MatExpansionModule, MatDatepickerModule, MatCheckboxModule, MatBadgeModule,MatCardModule, MatGridListModule, MatSelectModule, MatPaginatorModule, MatInputModule, MatFormFieldModule, MatListModule,BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatTableModule, MatMenuModule, MatTabsModule, MatDividerModule],
+  declarations: [AppComponent, PageNotFoundComponent, RetailerComponent, ShopperComponent, HomeComponent, ProductsComponent, AdminPanelComponent, ChatComponent, EventsComponent, SidebarComponent],
+  imports: [HttpClientModule, MatExpansionModule, MatDatepickerModule, MatCheckboxModule, MatBadgeModule, MatCardModule, MatGridListModule, MatSelectModule, MatPaginatorModule, MatInputModule, MatFormFieldModule, MatListModule, BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatTableModule, MatMenuModule, MatTabsModule, MatDividerModule],
   providers: [ChatServiceService],
   bootstrap: [AppComponent]
 })
