9575f63dc93976e8e6da93fbc1e85290f095de20
daniel <u202121440@upc.edu.pe>
2023-10-31 01:28:41 -0500

    feat(develop): added retailer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {PageNotFoundComponent} from './public/pages/page-not-found/page-not-foun
 import {RetailerComponent} from './rewear/retailers/pages/retailer/retailer.component';
 import {ShopperComponent} from "./rewear/shoppers/pages/shopper/shopper.component";
 import {HomeComponent} from './public/pages/home/home.component';
+import {ProductsComponent} from "./rewear/retailers/components/products/products.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatButtonModule} from "@angular/material/button";
@@ -29,11 +30,15 @@ import {MatExpansionModule} from '@angular/material/expansion';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatBadgeModule} from '@angular/material/badge';
-
+import { AdminPanelComponent } from './rewear/retailers/components/admin-panel/admin-panel.component';
+import { ChatComponent } from './rewear/retailers/components/chat/chat.component';
+import { EventsComponent } from './rewear/retailers/components/events/events.component';
+import {ChatServiceService} from "./rewear/retailers/services/chat-service.service";
+import {HttpClientModule} from "@angular/common/http";
 @NgModule({
-  declarations: [AppComponent, PageNotFoundComponent, RetailerComponent, ShopperComponent, HomeComponent],
-  imports: [ MatExpansionModule, MatDatepickerModule, MatCheckboxModule, MatBadgeModule,MatCardModule, MatGridListModule, MatSelectModule, MatPaginatorModule, MatInputModule, MatFormFieldModule, MatListModule,BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatTableModule, MatMenuModule, MatTabsModule, MatDividerModule],
-  providers: [],
+  declarations: [AppComponent, PageNotFoundComponent, RetailerComponent, ShopperComponent, HomeComponent, ProductsComponent, AdminPanelComponent, ChatComponent, EventsComponent],
+  imports: [ HttpClientModule,MatExpansionModule, MatDatepickerModule, MatCheckboxModule, MatBadgeModule,MatCardModule, MatGridListModule, MatSelectModule, MatPaginatorModule, MatInputModule, MatFormFieldModule, MatListModule,BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatTableModule, MatMenuModule, MatTabsModule, MatDividerModule],
+  providers: [ChatServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
