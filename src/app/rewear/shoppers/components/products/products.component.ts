e0df58172e5a8b289ae9363926ed5ead987141b5
Adrian Melgar <DaikiMT@outlook.es>
2023-10-30 23:52:54 -0500

    Implemented product component basic api calls

--- src/app/rewear/shoppers/components/products/products.component.ts
+++ src/app/rewear/shoppers/components/products/products.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Products} from "../../model/products";
+import {ProductsService} from "../../services/products.service";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: Products[] = [];
+  constructor(private productsService: ProductsService) { }
+  ngOnInit(): void {
+    this.productsService.getAll().subscribe((response: any) => {
+      console.log(response);
+      this.products = response;
+    });
+  }
 
 }
